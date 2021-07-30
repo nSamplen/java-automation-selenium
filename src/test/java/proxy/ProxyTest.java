4cf917717610f65fb87e9e19be033e7e9249e97f
Anna Egorova <anyaaaaegorova@gmail.com>
2021-07-30 16:54:55 +0300

    Proxy test

---
+++ src/test/java/proxy/ProxyTest.java
@@ -0,0 +1,50 @@
+package proxy;
+
+import net.lightbody.bmp.BrowserMobProxy;
+import net.lightbody.bmp.BrowserMobProxyServer;
+import net.lightbody.bmp.client.ClientUtil;
+import net.lightbody.bmp.core.har.Har;
+import net.lightbody.bmp.proxy.CaptureType;
+import org.openqa.selenium.Proxy;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.Test;
+
+import java.io.File;
+
+public class ProxyTest extends BaseTest{
+
+    @Test
+    public void bmpTest() throws Exception {
+
+        // lauching proxy server
+
+        // get selenium proxy
+        // start the proxy
+        BrowserMobProxy proxy = new BrowserMobProxyServer();
+        proxy.start(0);
+
+        // get the Selenium proxy object
+        Proxy seleniumProxy = ClientUtil.createSeleniumProxy(proxy);
+
+        // configure it as a desired capability
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(CapabilityType.PROXY, seleniumProxy);
+
+        // start the browser up
+        WebDriver driver = new ChromeDriver(capabilities);
+        // enable more detailed HAR capture, if desired (see CaptureType for the complete list)
+        proxy.enableHarCaptureTypes(CaptureType.REQUEST_CONTENT, CaptureType.RESPONSE_CONTENT);
+        proxy.newHar("testlink");
+        driver.get("http://172.29.160.1/");
+
+        Har har = proxy.getHar();
+        proxy.getHar().writeTo(new File("logs/Test.har"));
+
+        driver.quit();
+        proxy.stop();;
+
+    }
+}
